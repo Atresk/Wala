547dec4987c4bd5ca9c90f492adf423ee40bd9aa
Atresk <90079930+Atresk@users.noreply.github.com>
2022-08-04 21:34:07 +0800

    N

--- deploy-command.js
+++ deploy-command.js
@@ -1,15 +1,21 @@
-const { SlashCommandBuilder, Routes } = require('discord.js');
-const { REST } = require('@discordjs/rest');
+const fs = require('node:fs');
+const path = require('node:path');
 const dotenv = require('dotenv'); dotenv.config();
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord.js');
 
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('𝐖𝐚𝐥𝐚當前的ping值'),
-]
-	.map(command => command.toJSON());
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 rest.put(Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID), { body: commands })
-	.then(() => console.log('bibo! bibo! 應用程式命令已同步!'))
+	.then(() => console.log('bibo! bibo! 應用程式命令已註冊!'))
 	.catch(console.error);
--- index.js
+++ index.js
@@ -1,26 +1,41 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 const dotenv = require('dotenv'); dotenv.config();
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// 斜線命令處理
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	}
+	catch (error) {
+		console.error(error);
+		await interaction.reply({ content: '錯誤! 錯誤!', ephemeral: true });
+	}
+});
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
+
 client.login(process.env.TOKEN);
-
-client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const { commandName } = interaction;
-
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	}
-	else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	}
-	else if (commandName === 'user') {
-		await interaction.reply('User info.');
-	}
-});
-
