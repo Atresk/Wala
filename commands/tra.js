620a9e2a00940ac17e62a1223987df2f9ab2a9a6
Atresk <90079930+Atresk@users.noreply.github.com>
2023-03-08 14:50:28 +0800

    新增偵測語言譯中 ＆ 偵測語言譯英

---
+++ commands/tra.js
@@ -0,0 +1,22 @@
+const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
+const translate = require('translate-google');
+
+module.exports = {
+	data: new ContextMenuCommandBuilder()
+        .setName('翻譯 偵測語言譯英')
+	    .setType(ApplicationCommandType.Message),
+	async execute(interaction) {
+        const A = translate(`${interaction.targetMessage.content}`, {to: 'en'}).then(res => {
+            return res;
+        }).catch(err => {
+            console.error(err);
+        })
+
+        const printA = async () => {
+            const B = await A;
+            await interaction.reply({ content: `原始訊息: ${interaction.targetMessage.content}\n譯文: ${B}`, ephemeral: true });
+        }
+        
+        printA();
+	},
+};
